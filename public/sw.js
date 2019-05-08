94d0e48e6c8691911766a883d7c7f0f9ade9b1b3
Stanley Huang <huang.stanley050@gmail.com>
2019-05-08 18:54:11 +1000

    caching skeleton

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,44 @@
-
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener("install", function(event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open("static").then(cache => {
+      console.log("pre caching app shell: ", cache);
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/css/help.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      ]);
+      // cache.add("/");
+      // cache.add("/index.html");
+      // cache.add("/src/js/app.js");
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener("activate", function(event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
 });
